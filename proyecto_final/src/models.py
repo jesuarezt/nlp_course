f493bc5da2e4d75ebaaface1a6a897ecb414a82a
jesuarezt <jesussuarezt@gmail.com>
2022-11-13 16:39:19 -0500

    final project 1 point

---
+++ proyecto_final/src/models.py
@@ -0,0 +1,141 @@
+class RNN_BI(nn.Module):
+    def __init__(self, vocab_size, embed_dim, rnn_hidden_size, fc_hidden_size):
+        super().__init__()
+        self.embedding = nn.Embedding(vocab_size, 
+                                      embed_dim, 
+                                      padding_idx=0) 
+        self.rnn = nn.RNN(embed_dim, rnn_hidden_size, 
+                           batch_first=True, bidirectional=True)
+        self.fc1 = nn.Linear(rnn_hidden_size*2, fc_hidden_size)
+        self.relu = nn.ReLU()
+        self.fc2 = nn.Linear(fc_hidden_size, num_classes)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, text, lengths):
+        out = self.embedding(text)
+        out = nn.utils.rnn.pack_padded_sequence(out, lengths.cpu().numpy(), enforce_sorted=False, batch_first=True)
+        _, (hidden) = self.rnn(out)
+        out = torch.cat((hidden[-2, :, :], hidden[-1, :, :]), dim=1)
+        out = self.fc1(out)
+        out = self.relu(out)
+        out = self.fc2(out)
+        out = self.sigmoid(out)
+        return out
+##model1_1 = RNN_BI(vocab_size, embed_dim, rnn_hidden_size, fc_hidden_size) 
+##model1_1 = model1_1.to(device)
+
+
+class RNN_GRU(nn.Module):
+    def __init__(self, vocab_size, embed_dim, rnn_hidden_size, fc_hidden_size):
+        super().__init__()
+        self.embedding = nn.Embedding(vocab_size, 
+                                      embed_dim, 
+                                      padding_idx=0) 
+        self.rnn = nn.GRU(embed_dim, rnn_hidden_size,
+                           batch_first=True)
+        self.fc1 = nn.Linear(rnn_hidden_size, fc_hidden_size)
+        self.relu = nn.ReLU()
+        self.fc2 = nn.Linear(fc_hidden_size, num_classes)
+        self.sigmoid = nn.Sigmoid()
+        
+
+    def forward(self, text, lengths):
+        out = self.embedding(text)
+        out = nn.utils.rnn.pack_padded_sequence(out, lengths.cpu().numpy(), enforce_sorted=False, batch_first=True)
+        out, (hidden) = self.rnn(out)
+        out = hidden[-1, :, :]
+        out = self.fc1(out)
+        out = self.relu(out)
+        out = self.fc2(out)
+        out = self.sigmoid(out)
+        return out
+
+##model2 = RNN_GRU(vocab_size, embed_dim, rnn_hidden_size, fc_hidden_size) 
+##model2 = model2.to(device)
+
+class RNN_GRU_BI(nn.Module):
+    def __init__(self, vocab_size, embed_dim, rnn_hidden_size, fc_hidden_size):
+        super().__init__()
+        self.embedding = nn.Embedding(vocab_size, 
+                                      embed_dim, 
+                                      padding_idx=0) 
+        self.rnn = nn.GRU(embed_dim, rnn_hidden_size, 
+                           batch_first=True, bidirectional=True)
+        self.fc1 = nn.Linear(rnn_hidden_size*2, fc_hidden_size)
+        self.relu = nn.ReLU()
+        self.fc2 = nn.Linear(fc_hidden_size, num_classes)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, text, lengths):
+        out = self.embedding(text)
+        out = nn.utils.rnn.pack_padded_sequence(out, lengths.cpu().numpy(), enforce_sorted=False, batch_first=True)
+        _, (hidden) = self.rnn(out)
+        out = torch.cat((hidden[-2, :, :], hidden[-1, :, :]), dim=1)
+        out = self.fc1(out)
+        out = self.relu(out)
+        out = self.fc2(out)
+        out = self.sigmoid(out)
+        return out
+ 
+model2_1 = RNN_GRU_BI(vocab_size, embed_dim, rnn_hidden_size, fc_hidden_size) 
+model2_1 = model2_1.to(device)
+
+
+
+class RNN_LSTM(nn.Module):
+    def __init__(self, vocab_size, embed_dim, rnn_hidden_size, fc_hidden_size):
+        super().__init__()
+        self.embedding = nn.Embedding(vocab_size, 
+                                      embed_dim, 
+                                      padding_idx=0) 
+        self.rnn = nn.LSTM(embed_dim, rnn_hidden_size, 
+                           batch_first=True)
+        self.fc1 = nn.Linear(rnn_hidden_size, fc_hidden_size)
+        self.relu = nn.ReLU()
+        self.fc2 = nn.Linear(fc_hidden_size, num_classes)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, text, lengths):
+        out = self.embedding(text)
+        out = nn.utils.rnn.pack_padded_sequence(out, lengths.cpu().numpy(), enforce_sorted=False, batch_first=True)
+        out, (hidden, cell) = self.rnn(out)
+        out = hidden[-1, :, :]
+        out = self.fc1(out)
+        out = self.relu(out)
+        out = self.fc2(out)
+        out = self.sigmoid(out)
+        return out
+
+
+
+#model3 = RNN_LSTM(vocab_size, embed_dim, rnn_hidden_size, fc_hidden_size) 
+#model3 = model3.to(device)
+
+
+class RNN_LSTM_BI(nn.Module):
+    def __init__(self, vocab_size, embed_dim, rnn_hidden_size, fc_hidden_size):
+        super().__init__()
+        self.embedding = nn.Embedding(vocab_size, 
+                                      embed_dim, 
+                                      padding_idx=0) 
+        self.rnn = nn.LSTM(embed_dim, rnn_hidden_size, 
+                           batch_first=True, bidirectional=True)
+        self.fc1 = nn.Linear(rnn_hidden_size*2, fc_hidden_size)
+        self.relu = nn.ReLU()
+        self.fc2 = nn.Linear(fc_hidden_size, num_classes)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, text, lengths):
+        out = self.embedding(text)
+        out = nn.utils.rnn.pack_padded_sequence(out, lengths.cpu().numpy(), enforce_sorted=False, batch_first=True)
+        _, (hidden, cell) = self.rnn(out)
+        out = torch.cat((hidden[-2, :, :], hidden[-1, :, :]), dim=1)
+        out = self.fc1(out)
+        out = self.relu(out)
+        out = self.fc2(out)
+        out = self.sigmoid(out)
+        return out
+    
+
+#model3_1 = RNN_LSTM_BI(vocab_size, embed_dim, rnn_hidden_size, fc_hidden_size) 
+#model3_1 = model3_1.to(device)
